4e0d1bece88d23b44658679159d554b31d681306
sam <botjes2@hotmail.com>
2019-02-07 21:57:16 +0100

    hello

--- rootfs/etc/owncloud.d/5-base-chown.sh
+++ rootfs/etc/owncloud.d/5-base-chown.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find /mnt \( \! -user www-data -o \! -group root \) -print0 | xargs -r -0 chown www-data:root
+find /mnt \( \! -user root -o \! -group root \) -print0 | xargs -r -0 chown root:root
